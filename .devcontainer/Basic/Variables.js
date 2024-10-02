533e672c2e371c8fec04d3adf1bb9a791427a4db
Shivam Upadhyay <129720997+ShivamUpadhyay12@users.noreply.github.com>
2024-10-02 17:22:12 +0000

    Container Config

---
+++ .devcontainer/Basic/Variables.js
@@ -0,0 +1,19 @@
+const accountId = 14567
+let accountEmail = "shivam@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState
+//accountId=5// not allow because const 
+console.log(accountId);
+accountEmail="sonu@gmail.com"
+accountPassword ="4321"
+accountCity ="Bengaluru"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+P<refer not to use var
+because of issue in block scope and functional scope
+*/
