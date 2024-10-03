a3db586ebf8bb1c1bb658c69827910ce9099b50d
Shivam Upadhyay <129720997+ShivamUpadhyay12@users.noreply.github.com>
2024-10-03 17:31:58 +0000

    Conversion of dataType

---
+++ .devcontainer/Basic/03_conversionOperation.js
@@ -0,0 +1,55 @@
+// let score = "33abc";
+// console.log(typeof score)//string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)//number
+// console.log(valueInNumber)//NaN
+//--------------
+// let score1 = null;
+// console.log(typeof score1)//object
+
+// let valueInNumber1 = Number(score1)
+// console.log(typeof valueInNumber1)//number
+// console.log(valueInNumber1)//0
+//---------------
+// let score2 = undefined;
+// console.log(typeof score2)//undefined
+
+// let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber2)//number
+// console.log(valueInNumber2)//Nan
+//------------
+// let score3 = true;
+//  console.log(typeof score3)//boolean
+// let valueInNumber3 = Number(score3)
+// console.log(typeof valueInNumber3)//number
+// console.log(valueInNumber3)//1
+//---------
+//  let score4 = "Shivam";
+// console.log(typeof score4)//string
+// let valueInNumber4 = Number(score4)
+// console.log(typeof valueInNumber4)//number
+// console.log(valueInNumber4)//NaN
+//--------------
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+//  true => 1; false => 0
+
+let isLoggedIn = "shivam"
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);//true
+
+let isLoggedIn1 = ""
+
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+//console.log(booleanIsLoggedIn1);//false
+
+// 1 => true; 0 => false
+// "" => false
+// "shivam" => true
+
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+ //console.log(stringNumber);//33
+ //console.log(typeof stringNumber);//string
