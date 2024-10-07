bb53e05a77b702b284ff37efd2edf1ce9652e4d6
Shivam Upadhyay <129720997+ShivamUpadhyay12@users.noreply.github.com>
2024-10-07 18:39:43 +0000

    Stackandheap

---
+++ .devcontainer/Basic/StackandHeap.js
@@ -0,0 +1,17 @@
+//Stack(Primitive), Heap(Non-Premitive)
+let myName = "shivamUpadhyay";
+let anotherNmae = myName
+anotherName = "sonuUpHathras"
+console.log(anotherName)
+console.log(myName)
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email ="sonu@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
