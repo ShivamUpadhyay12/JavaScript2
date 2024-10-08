707e13562c0fdbcb3017fd6e7c0636e7f76c63c4
Shivam Upadhyay <129720997+ShivamUpadhyay12@users.noreply.github.com>
2024-10-08 18:17:51 +0000

    String

---
+++ .devcontainer/Basic/05_String.js
@@ -0,0 +1,30 @@
+const name ="shivam-su";
+const surname ="upadhyay";
+//console.log(name + surname +"Jaipur")
+//console.log(`My name is ${name} ${surname} `)
+const gameName = new String('Shivam')
+//console.log(gameName[0]);//S
+//console.log(gameName.__proto__);//{}
+
+//console.log(gameName.length); //6
+//console.log(gameName.toUpperCase());//SHIVAM
+//console.log(gameName.charAt(4));//a
+//console.log(gameName.indexOf('i'));//2
+//const newString = gameName.substring(0,4)
+//console.log(newString)//Shiv
+//const anotherString = gameName.substring(-3,4)
+//console.log(anotherString);//Shiv
+ //const newStringOne =" shivam "
+ //console.log(newStringOne);
+ console.log(newStringOne.trim());
+
+ //const url ="https://github.com/ShivamUpadhyay12"
+ //console.log(url.replace('12','@#'))
+
+
+ //console.log(url.includes('Shivam'))
+
+ console.log(name.split('-'))
+
+
+
